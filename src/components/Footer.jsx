e284fbe224fbea33d92766905de64dcde54f1170
kumar-pratik1006 <kumarpratik1006@gmail.com>
2025-04-01 11:13:49 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="w-full py-4 bg-black-100 text-white flex justify-center gap-6">
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram size={24} />
+      </a>
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaXTwitter size={24} />
+      </a>
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin size={24} />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
